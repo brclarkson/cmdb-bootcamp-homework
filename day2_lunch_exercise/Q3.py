518f90e68d53e06322745a90692d47cbea3c05c3
Benjamin <bclarks1@jhu.edu>
2014-09-03 14:01:01 -0400

    Added comment to Day2 lunch Q3

--- day2_lunch_exercise/Q3.py
+++ day2_lunch_exercise/Q3.py
@@ -8,6 +8,7 @@ f = open(sam_file)
 
 count = 0
 for line in f:
+    # NH:i:1 is the format of the label we found on google that corresponds to a read mapped to exactly one location on genome
     if "NH:i:1" in line:
         count = count + 1
 print count
