f61057f6e8601ff5a54d3547f21b40a539f29372
Benjamin <bclarks1@jhu.edu>
2014-09-03 22:49:05 -0400

    Day2 HW Q3 code and plot

---
+++ day2_HW/Q3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# I have no headers in my csv file, so I need to specify that so pandas doesn't call the first row a header
+sxl_df = pd.read_table("/Users/cmdb/cmdb-bootcamp-homework/day2_HW/female_sxl.csv", header = None)
+
+
+plt.figure()
+# FPKM data is contained in column 9, so I specify that I want the 9th value in each row for the y data
+sxl_df.plot( y=9 )
+plt.savefig("female_sxl_plot.png")
